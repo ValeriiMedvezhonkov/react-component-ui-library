7acbd0773ad044e75c973b5d7b39a6729a505e25
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-04-05 01:30:17 +0200

    create starter gh-page

--- src/index.tsx
+++
@@ -1,4 +0,0 @@
-import Alert from './components/Alert'
-import { SnackbarProvider, useSnackbar } from './components/Snackbar/SnackbarProvider'
-import Loader from './components/Loader/Loader'
-export { Alert, SnackbarProvider, useSnackbar, Loader }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        green: {
-          dark: 'rgb(var(--color-green-dark))',
-          base: 'rgb(var(--color-green-base))',
-          'base-35': 'rgb(var(--color-green-base-35))',
-          light: 'rgb(var(--color-green-light))',
-        },
-        red: {
-          dark: 'rgb(var(--color-red-dark))',
-          base: 'rgb(var(--color-red-base))',
-          'base-35': 'rgb(var(--color-red-base-35))',
-          light: 'rgb(var(--color-red-light))',
-        },
-        yellow: {
-          dark: 'rgb(var(--color-yellow-dark))',
-          base: 'rgb(var(--color-yellow-base))',
-          'base-35': 'rgb(var(--color-yellow-base-35))',
-          light: 'rgb(var(--color-yellow-light))',
-        },
-        blue: {
-          dark: 'rgb(var(--color-blue-dark))',
-          base: 'rgb(var(--color-blue-base))',
-          'base-35': 'rgb(var(--color-blue-base-35))',
-          light: 'rgb(var(--color-blue-light))',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
