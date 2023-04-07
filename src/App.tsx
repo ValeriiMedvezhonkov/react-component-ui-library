5b85be09c5dc50df16182b84f4dbb593a51bea52
Valerii Medvezhonkov <medvezhonkov285@gmail.com>
2023-04-07 23:18:45 +0200

    change page title and fix base url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className='bg-gray-200 min-h-screen flex justify-center items-center'>
-      <h1 className='text-3xl font-bold text-blue-600'>Coming soon ...</h1>
+      <h1 className='text-3xl font-bold text-blue-900'>Coming soon ...</h1>
     </div>
   )
 }
